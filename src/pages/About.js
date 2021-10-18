27ec5e70a5b164bb10d4c87cdf7966ffeaeaff52
Francisco <fnaveira11@gmail.com>
2021-10-17 22:05:37 -0400

    Added personal info to the about-details section. Also styled the about-details section.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from '../components/Title';
+import ImageSection from './ImageSection';
 
 function About() {
 
     return (
         <div className="about-page">
             <Title title={'About Me-test'} span={'About Me-test'}/>
+            <ImageSection/>
         </div>
     )
 }
--- src/pages/ImageSection.js
+++ src/pages/ImageSection.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import AboutPic from '../img/AboutPic';
+import AboutPic from '../img/AboutPic.jpg'
 
 function ImageSection() {
     return (
-        <div className='ImageSection'>
-            <div className='img'>
+        <div className='image-section'>
+            <div className='img-dev'>
                 {/* Replace this with a better picture later */}
-                <img  src={AboutPic} alt="about-img"/>
+                <img src={AboutPic} alt=''/>
             </div>
             <div className='about-info'>
                 <h4>I am <span>Francisco Naveira</span></h4>
-                <p>
+                <p className='about-text'>
                     About This will be a paragraph talking about who francisco is and how he is super cool.
                     About This will be a paragraph talking about who francisco is and how he is super cool.
                     About This will be a paragraph talking about who francisco is and how he is super cool.
                 </p>   
                 <div className='about-details'>
-                    
+                    <div className="about-details-left">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Languages</p>
+                        <p>Location</p>
+                    </div>
+                    <div className="about-details-right">
+                        <p>: Francisco Naveira</p>
+                        <p>: 28</p>
+                        <p>: English and Spanish</p>
+                        <p>: Elon, North Carolina</p>
+                    </div>
                 </div>
+                <button className='resume-btn'>Download Resume</button>
             </div>
         </div>
     )

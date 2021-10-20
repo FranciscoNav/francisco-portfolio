14f6e4ddb2d36834977df107be4433b32b5b7df0
Francisco <fnaveira11@gmail.com>
2021-10-19 22:21:06 -0400

    finished the styling of the skills-section

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,14 @@ function About() {
             <Title title={'About'} span={'About'}/>
             <ImageSection/>
             <Title title={'My Skills'} span={'My Skills'}/>
-            <SkillsSection skill={'Javascript'} progress={'70%'}/>
+            <div className='skillsSection-container'>
+                <SkillsSection skill={'Javascript'} progress={'75%'}/>
+                <SkillsSection skill={'React'} progress={'80%'}/>
+                <SkillsSection skill={'Ruby on rails'} progress={'60%'}/>
+                <SkillsSection skill={'Ruby'} progress={'60%'}/>
+                <SkillsSection skill={'SQL'} progress={'40%'}/>
+                <SkillsSection skill={'HTML'} progress={'55%'}/>
+            </div>
         </div>
     )
 }

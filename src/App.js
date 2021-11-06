fa33cd47f2060c57c2ad780885ce4b1fc2a71a70
Francisco <fnaveira11@gmail.com>
2021-11-05 22:29:19 -0400

    Added column flex to improve about page styleing when being used on mobile device

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import About from './pages/About';
 import Blogs from './pages/Blogs';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
 
   return (
     <div className="App">
-      <div className="side-bar">
+      <div className={`side-bar ${navToggle? 'bar-toggle':''}`}>
         <NavBar/>
       </div>
-      <div className="nav-button">
+      <div className="nav-button" onClick={()=>{setNavToggle(!navToggle)}}>
         <div className="line-1"></div>
         <div className="line-2"></div>
         <div className="line-3"></div>

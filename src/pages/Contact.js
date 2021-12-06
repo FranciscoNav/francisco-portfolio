efb772f4b7a4735dc35c218992be0b7a6d890385
Francisco <fnaveira11@gmail.com>
2021-12-06 17:44:29 -0500

    Added fully functioning contect form so users can email me directly. Also added style for sed form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,20 @@ function Contact() {
     return (
         <div className='contact-page'>
             <Title title={'Contact'} span={'Contact'}/>
-            <p>Add form here</p>
+            <br/>
+            <p>If you would like to contact me, or if you found a bug on the site, please fill out the form below. Thank you!</p>
+            <br/>
+            <form action="https://formsubmit.co/fnaveira11@gmail.com" method="POST" className="contact-form">
+                <h5>Contact Form</h5>
+                <input type="text" name="name" placeholder="Name" required className="contact-input"/>
+                <br/>
+                <input type="email" name="email" placeholder="Email" required className="contact-input"/>
+                <br/>
+                <textarea type="text" name="content" placeholder="Message"  className="contact-message"></textarea>
+                <br/>
+                <input type="hidden" name="_next" value="http://localhost:3000/contact"/>
+                <button type='submit' className="contact-button">Send</button>
+            </form>
         </div>
     )
 }
